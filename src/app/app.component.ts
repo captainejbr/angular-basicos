ce3bb1875065cf2b19a90031e2b9d82a3f24ceda
Eli Boscan <eli.boscan@farmatodo.com>
2022-07-10 15:28:02 -0400

    leccion 43

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html'
+
 })
 export class AppComponent {
-  title = 'bases';
+  public titulo: string = 'Contandor App';
+  numero: number = 10;
+
+ base: number = 5;
+
+  acumular(valor: number){
+    this.numero += valor;
+  }
+
 }
